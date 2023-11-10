0032a188692967e40ff09d929d3e036ff51102fd
Mathieu Jugi <mathieujugi@gmail.com>
2023-11-10 16:55:52 +0100

    correction image + ajout nodemon packagejson

--- back/Middlewares/multerMid.js
+++ back/Middlewares/multerMid.js
@@ -1,13 +1,14 @@
 const multer = require("multer");
+const uuid = require("uuid");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "../images/produits");
   },
   filename: function (req, file, cb) {
-    const productTitre = req.body.titre;
+    const idUnique = uuid.v4();
     const fileExtension = file.mimetype.split("/")[1];
-    const newFileTitre = `${productTitre}.${fileExtension}`;
+    const newFileTitre = `${idUnique}.${fileExtension}`;
 
     cb(null, `${newFileTitre}`);
   },
