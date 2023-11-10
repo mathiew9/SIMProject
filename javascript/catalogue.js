a4bf2d4a2a178d9eb461364daee59803f261e547
Mathieu Jugi <mathieujugi@gmail.com>
2023-11-10 16:10:59 +0100

    ajout form post p2

--- javascript/catalogue.js
+++ javascript/catalogue.js
@@ -1,26 +1,42 @@
 let dernierePositionScroll = 0;
 
 function positionZero(scrollPos) {
-  if(scrollPos == 0){
-    $('.retour_haut').addClass('scroll_down'); 
+  if (scrollPos == 0) {
+    $(".retour_haut").addClass("scroll_down");
+  } else {
+    $(".retour_haut").removeClass("scroll_down");
   }
-  else{
-    $('.retour_haut').removeClass('scroll_down');
-}
 }
 
 document.addEventListener("scroll", (event) => {
   dernierePositionScroll = window.scrollY;
-    positionZero(dernierePositionScroll);
+  positionZero(dernierePositionScroll);
 });
 
-
 function ouvrirBurger() {
-    document.getElementById("monMenuBurger").style.width = "250px";
-    $('body > *:not(.menuburger)').addClass('filtreflou'); 
-  }
+  document.getElementById("monMenuBurger").style.width = "250px";
+  $("body > *:not(.menuburger)").addClass("filtreflou");
+}
 
 function fermerBurger() {
-    document.getElementById("monMenuBurger").style.width = "0";
-    $('body > *:not(.menuburger)').removeClass('filtreflou'); 
-  }
+  document.getElementById("monMenuBurger").style.width = "0";
+  $("body > *:not(.menuburger)").removeClass("filtreflou");
+}
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("produitForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const formData = new FormData(this);
+      console.log(formData);
+      fetch("http://localhost:3000/product", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => {
+          console.error("Fetch error:", err);
+        });
+    });
+});
